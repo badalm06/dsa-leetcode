c508a8162b63f56a6dd281edb7cce8679f94463c
Badal Sharma <96584121+badalm06@users.noreply.github.com>
2026-07-31 02:06:36 +0530

    Time: 0 ms (100.00%) | Memory: 25.9 MB (78.49%) - LeetSync

---
+++ two-pointer/42-trapping-rain-water/trapping-rain-water.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int trap(vector<int>& height) {
+        int left = 0;
+        int right = height.size()-1;
+
+        int leftMax = 0, rightMax = 0, totalWater = 0;
+        while(left<right){
+            leftMax = max(leftMax, height[left]);
+            rightMax = max(rightMax, height[right]);
+
+            if(leftMax < rightMax){
+                totalWater += leftMax-height[left];
+                left++;
+            }
+            else{
+                totalWater += rightMax - height[right];
+                right--;
+            }
+        }
+        return totalWater;
+    }
+};
