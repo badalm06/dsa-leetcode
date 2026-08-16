14a29b5e2f2882c283546b01960db445bd3a8927
Badal Sharma <96584121+badalm06@users.noreply.github.com>
2026-08-16 15:09:21 +0530

    Time: 71 ms (94.11%) | Memory: 146.4 MB (26.57%) - LeetSync

---
+++ sql/523-continuous-subarray-sum/continuous-subarray-sum.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    bool checkSubarraySum(vector<int>& nums, int k) {
+        if(nums.size()<2) return false;
+        int sum = 0;
+        unordered_map<int,int> mpp;
+        mpp[0] = -1;
+
+        for(int i=0;i<nums.size();i++) {
+            sum += nums[i];
+            if(k != 0) sum = sum % k;
+
+            if(mpp.find(sum) != mpp.end()) {
+                if(i-mpp[sum]>1) return true;
+            }
+            else{
+                mpp[sum] = i;
+            }
+        }
+        return false;
+    }
+};
