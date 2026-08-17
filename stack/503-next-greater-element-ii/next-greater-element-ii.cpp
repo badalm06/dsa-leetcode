dbbac98b368544588b8d0c5ab5a1ec8a233ca001
Badal Sharma <96584121+badalm06@users.noreply.github.com>
2026-08-18 02:05:36 +0530

    Time: 4 ms (59.23%) | Memory: 29 MB (14.86%) - LeetSync

--- stack/503-next-greater-element-ii/next-greater-element-ii.cpp
+++ stack/503-next-greater-element-ii/next-greater-element-ii.cpp
@@ -1,3 +1,4 @@
+// Using two loops
 class Solution {
 public:
     vector<int> nextGreaterElements(vector<int>& nums) {
@@ -5,15 +6,17 @@ public:
         vector<int> res(n,-1);
         stack<int> st;
 
-        for(int i=2*n-2;i>=0;i--) {
-            int index = i%n;
-            while(!st.empty() && st.top() <= nums[index]) {
+        for(int i=n-1;i>=0;i--) {
+            st.push(nums[i]);
+        }
+        for(int i=n-1;i>=0;i--) {
+            while(!st.empty() && st.top() <= nums[i]) {
                 st.pop();
             }
-            if(i<n) {
-                if(!st.empty()) res[index] = st.top();
-            }
-            st.push(nums[index]);
+          
+            if(!st.empty()) res[i] = st.top();
+            
+            st.push(nums[i]);
         }
         return res;
     }
