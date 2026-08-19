8473d3681cd103a5f7bc9155c5fc8b8d1cb21e73
Badal Sharma <96584121+badalm06@users.noreply.github.com>
2026-08-20 02:55:40 +0530

    Time: 0 ms (100.00%) | Memory: 8.9 MB (42.72%) - LeetSync

--- stack/1297-maximum-number-of-balloons/maximum-number-of-balloons.cpp
+++ stack/1297-maximum-number-of-balloons/maximum-number-of-balloons.cpp
@@ -1,26 +1,17 @@
 class Solution {
 public:
     int maxNumberOfBalloons(string text) {
-        unordered_map<char,int> mp;
-        for(int i=0;i<text.length();i++) {
-            mp[text[i]]++;
-        }
+        vector<int> count(26,0);
 
-        unordered_map<char,int> mpp2;
-            mpp2['b'] = 1;
-            mpp2['a'] = 1;
-            mpp2['l'] = 2;
-            mpp2['o'] = 2;
-            mpp2['n'] = 1;
-        
-        int res = INT_MAX;
-        for(auto i:mpp2) {
-            char c = i.first;
-            int fmpp2 = i.second;
-            int fmp = mp[c];
-            int times = fmp/fmpp2;
-            res = min(res,times);
+        for(char c: text){
+            count[c - 'a']++;
         }
-        return res;
+        return min({
+            count['b' - 'a'],
+            count['a' - 'a'],
+            count['l' - 'a']/2,
+            count['o' - 'a']/2,
+            count['n' - 'a']
+        });
     }
 };
