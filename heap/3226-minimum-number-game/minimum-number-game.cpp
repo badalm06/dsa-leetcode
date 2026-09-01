cb198aaf1680dcea11cfe5ee251753c79ad6292d
Badal Sharma <96584121+badalm06@users.noreply.github.com>
2026-09-01 23:25:41 +0530

    Time: 1 ms (31.61%) | Memory: 28.7 MB (5.13%) - LeetSync

---
+++ heap/3226-minimum-number-game/minimum-number-game.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    vector<int> numberGame(vector<int>& nums) {
+        priority_queue<int,vector<int>,greater<int>>pq;
+        for(int num:nums) {
+            pq.push(num);
+        }
+        vector<int> ans;
+        while(!pq.empty()) {
+            int a = pq.top();
+            pq.pop();
+            int b = pq.top();
+            pq.pop();
+
+            ans.push_back(b);
+            ans.push_back(a);
+        }
+        return ans;
+    }
+};
