6316e3d0b2ca6803d241d0f1738f37c9bd12766c
Badal Sharma <96584121+badalm06@users.noreply.github.com>
2026-09-09 03:13:22 +0530

    Time: 0 ms (100.00%) | Memory: 8.7 MB (60.88%) - LeetSync

---
+++ recursion/216-combination-sum-iii/combination-sum-iii.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    void solve(int idx, int k, int n, vector<int>& temp,
+               vector<vector<int>>& ans) {
+
+        if(temp.size()==k) {
+            if(n==0) {
+                ans.push_back(temp);
+                return;
+            }
+        }
+    
+        for(int i=idx;i<=9;i++) {
+            if(i>n) break;
+            temp.push_back(i);
+            solve(i+1,k,n-i,temp,ans);
+            temp.pop_back();
+        }
+
+    }
+    vector<vector<int>> combinationSum3(int k, int n) {
+        vector<vector<int>>ans;
+        vector<int> temp;
+        solve(1,k,n,temp,ans);
+
+        return ans;
+    }
+};
