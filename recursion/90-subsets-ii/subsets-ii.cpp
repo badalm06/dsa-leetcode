2c2561d1e218b8bca37cc6369ea3b5ebda134422
Badal Sharma <96584121+badalm06@users.noreply.github.com>
2026-09-09 02:49:44 +0530

    Time: 2 ms (39.21%) | Memory: 10.4 MB (84.54%) - LeetSync

---
+++ recursion/90-subsets-ii/subsets-ii.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+
+    void solve(vector<int>& nums, int idx, vector<int>& temp, vector<vector<int>>& ans) {
+        ans.push_back(temp);
+
+        for(int i=idx;i<nums.size();i++) {
+            if(i>idx && nums[i] == nums[i-1]) continue;
+
+            temp.push_back(nums[i]);
+            solve(nums,i+1,temp,ans);
+            temp.pop_back();
+        }
+    }
+
+    vector<vector<int>> subsetsWithDup(vector<int>& nums) {
+        sort(nums.begin(),nums.end());
+        vector<vector<int>> ans;
+        vector<int> temp;
+
+        solve(nums,0,temp,ans);
+
+        return ans;
+    }
+};
